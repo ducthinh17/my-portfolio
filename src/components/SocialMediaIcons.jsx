936a7afabb2724122c67ec818a29cdd1b60deb59
Thinh <pldthinh.ityu@gmail.com>
2023-06-07 01:06:19 +0700

    add all

---
+++ src/components/SocialMediaIcons.jsx
@@ -0,0 +1,26 @@
+const SocialMediaIcons = () => {
+  return (
+    <div className="flex justify-center md:justify-start my-10 gap-7">
+      <a
+        className="hover:opacity-50 transition duration-500"
+        href="https://www.linkedin.com/in/pham-le-duc-thinh-4095aa273/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img alt="linkedin-link" src="../assets/linkedin.png" />
+      </a>
+     
+      <a
+        className="hover:opacity-50 transition duration-500"
+        href="https://www.facebook.com/ducthinh.pld"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img alt="facebook-link" src="../assets/facebook.png" />
+      </a>
+      
+    </div>
+  );
+};
+
+export default SocialMediaIcons;
---
+++ src/scenes/Testimonials.jsx
@@ -0,0 +1,94 @@
+import LineGradient from "../components/LineGradient";
+import { motion } from "framer-motion";
+
+const Testimonials = () => {
+  return (
+    <section id="testimonials" className="pt-32 pb-16">
+      {/* HEADING */}
+      <motion.div
+        className="md:w-1/3 text-center md:text-left"
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true, amount: 0.5 }}
+        transition={{ duration: 0.5 }}
+        variants={{
+          hidden: { opacity: 0, x: -50 },
+          visible: { opacity: 1, x: 0 },
+        }}
+      >
+        <p className="font-playfair font-semibold text-4xl mb-5 text-red">
+          TESTIMONIALS
+        </p>
+        <LineGradient width="mx-auto w-2/5" />
+        <p className="mt-10">Here's What US are Saying About My LOVE.</p>
+      </motion.div>
+
+      {/* TESTIMONIALS */}
+      <div className="md:flex md:justify-between gap-8">
+        <motion.div
+          className="mx-auto relative bg-blue max-w-[400px] h-[350px] flex flex-col justify-end p-16 mt-48
+            before:absolute before:top-[-120px] before:-ml-[110px] before:left-1/2 before:content-person1"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 0.6 }}
+          variants={{
+            hidden: { opacity: 0, scale: 0.8 },
+            visible: { opacity: 1, scale: 1 },
+          }}
+        >
+          
+          <p className="font-playfair text-6xl">“</p>
+          <p className="text-center text-xl">
+          "Muốn ngồi ở vị trí không ai ngồi được, bạn phải chịu được những cảm giác không ai chịu được." 
+          <br />
+          
+                - Sơn Tùng MTP -
+                
+          </p>
+        </motion.div>
+
+        <motion.div
+          className="mx-auto relative bg-red max-w-[400px] h-[350px] flex flex-col justify-end p-16 mt-48
+            before:absolute before:top-[-120px] before:-ml-[110px] before:left-1/2 before:content-person2"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ delay: 0.2, duration: 0.6 }}
+          variants={{
+            hidden: { opacity: 0, scale: 0.8 },
+            visible: { opacity: 1, scale: 1 },
+          }}
+        >
+          <p className="font-playfair text-6xl">“</p>
+          <p className="text-center text-xl">
+          "The best way to predict the future is to create it."
+          < br /> - Peter Diamandis -
+          </p>
+        </motion.div>
+
+        <motion.div
+          className="mx-auto relative bg-yellow max-w-[400px] h-[350px] flex flex-col justify-end p-16 mt-48
+            before:absolute before:top-[-120px] before:-ml-[110px] before:left-1/2 before:content-person3"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ delay: 0.4, duration: 0.6 }}
+          variants={{
+            hidden: { opacity: 0, scale: 0.8 },
+            visible: { opacity: 1, scale: 1 },
+          }}
+        >
+          <p className="font-playfair text-6xl">“</p>
+          <p className="text-center text-xl">
+          "The only limit to our realization of tomorrow will be our doubts of today." 
+          <br />
+          - Elon Musk -
+          </p>
+        </motion.div>
+      </div>
+    </section>
+  );
+};
+
+export default Testimonials;
